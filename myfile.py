02906f17787881739ad17931130b7645660e9969
orewanf <orewanf123@gmail.com>
2015-09-11 19:47:13 +0700

    adding module add_two_numbers

--- myfile.py
+++ myfile.py
@@ -1,2 +1,12 @@
-def do_something(number):
-    return 22 + number
+"""myfile."""
+
+
+def add_two_numbers(a, b):
+    """Adds x and y."""
+    addition = a + b
+    return addition
+
+if __name__ == '__main__':
+    a = 5
+    b = 7
+    print(add_two_numbers(a, b))
