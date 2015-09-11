1f51d6a271a82bfbb14e83feaa18c25ccf901bc2
orewanf <orewanf123@gmail.com>
2015-09-11 12:30:17 +0700

    changing the message

--- myfile.py
+++ myfile.py
@@ -1,10 +1,2 @@
-"""myfile."""
-# Copyright (c) 2015 Copyright Holder
-# hahaha
-
-CONSTANT = 5
-
-
-def do_somethin(number):
-    """Return the pathname of the KOS root directory."""
+def do_something(number):
     pass
